a5cdcc402a5a2dad353e31a80547b14237e9210d
gauravgb21 <sharmagaurav7892@gmail.com>
2020-12-23 12:51:29 +0530

    ishot screenshots added

--- client/src/components/ProfileDetails.js
+++ client/src/components/ProfileDetails.js
@@ -4,7 +4,7 @@ const ProfileDetails = props => {
     return(
         <div className={'isht-profile-user-details'}>
             <div className={'isht-profile-pic-cont'}>
-              <img alt='profile pic' src='https://instagram.fdel1-4.fna.fbcdn.net/v/t51.2885-19/s150x150/117171619_729277130966509_8911769517656992152_n.jpg?_nc_ht=instagram.fdel1-4.fna.fbcdn.net&_nc_ohc=h-Xirz3fQBAAX8CQ6DY&oh=9b94f5781f2554ff51ce6bd80d5f29de&oe=5FA7BD7B'/>
+              <img alt='profile pic' src='https://miro.medium.com/max/1000/0*kBHpKva09AsGj7RQ' alt='profile picture'/>
             </div>
             <div className={'isht-user-info-cont'}>
                 <div className={'isht-user-info-username'}>
@@ -18,7 +18,7 @@ const ProfileDetails = props => {
                 </div>
                 <div className={'isht-basic-user-info-cont'}>
                     <div className={'isht-user-info-name'}>{'Gaurav Bisht'}</div>
-                    <div className={'isht-user-bio'}>{'Viscal El Barca \n Bitch be humble'}</div>
+                    <div className={'isht-user-bio'}>{'Viscal El Barca'}</div>
                 </div>
             </div>
         </div>
